caba5ab7e82ec993a51978ce4802636a00f8d6b2
parkgain <pgi082247@gmail.com>
2023-03-01 14:53:07 +0900

    docs : 오타 수정 후 홈페이지 개편 작업 시작

--- fonts/font.jsx
+++ fonts/font.jsx
@@ -1,13 +1,13 @@
 import { Nanum_Gothic, Noto_Sans_KR } from "@next/font/google";
 
 export const nanum_gothic = Nanum_Gothic({
-  subsets: ["korean", "latin"],
+  subsets: ["latin"],
   weight: ["700"],
   fallback: ["system-ui", "arial"],
 });
 
 export const noto_sans_korean = Noto_Sans_KR({
-  subsets: ["korean"],
+  subsets: ["latin"],
   weight: ["300", "400"],
   fallback: ["system-ui", "arial"],
 });
