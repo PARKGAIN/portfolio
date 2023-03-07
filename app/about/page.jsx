984001e73277f3365f13fde09891bad17d309b1a
parkgain <pgi082247@gmail.com>
2023-03-07 23:49:31 +0900

    docs: navbar 없앰

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,19 +1,14 @@
 "use client";
 import React from "react";
-import Navbar from "../../components/Navbar";
 import Image from "next/image";
 import TechStackIcons from "../../components/TechStackIcons";
 import { ThemeProvider } from "next-themes";
-import { visibility } from "../../context/context";
 import AboutTxt from "../../components/AboutTxt";
-import Snowfall from "react-snowfall";
 
 function Page() {
   return (
     <div>
       <ThemeProvider attribute="class">
-        <Navbar visibility={visibility} />
-        <Snowfall />
         <h3 className="text-2xl uppercase top-24 text-gray-500 text-center mb-12 mt-6">
           About
         </h3>
@@ -22,8 +17,7 @@ function Page() {
           alt="myprofileimg"
           width={200}
           height={300}
-          className="mb-20 w-56 h-56 rounded-full object-cover md:rounded-lg m-auto"
-        />
+          className="mb-20 w-56 h-56 rounded-full object-cover md:rounded-lg m-auto"/>
         <AboutTxt />
         <h3 className="text-center mb-1">기술스택</h3>
         <TechStackIcons />
--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,11 @@
 "use client";
 import React from "react";
-import { ThemeProvider } from "next-themes";
-import Navbar from "../components/Navbar";
-import { visibility } from "../context/context";
 import CardView from "../components/CardView";
 React.useLayoutEffect = React.useEffect;
 
 function Page() {
   return (
     <div>
-        <Navbar visibility={visibility} />
         <CardView/>
     </div>
   );
