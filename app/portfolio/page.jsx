29b937a21d25df5cb3890311b08daed751511cc4
parkgain <pgi082247@gmail.com>
2023-03-12 20:54:58 +0900

    feat: 구글 ga 추가

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -4,9 +4,21 @@ import Navbar from "../../components/Navbar";
 import CardView from "../../components/CardView";
 import { ThemeProvider } from "next-themes";
 import { visibility } from "../../context/context";
+import Script from "next/script";
 function Page() {
   return (
     <div>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-36GQRC58RX"/>
+      <Script id="google-analytics">  
+      {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-36GQRC58RX');
+      `
+      }
+      </Script>
       <ThemeProvider attribute="class">
         <Navbar visibility={!visibility} />
         <CardView />
