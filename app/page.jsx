614ae15b10227e3f8ec09d0c97049e0d754ddfca
parkgain <pgi082247@gmail.com>
2023-03-12 21:12:04 +0900

    docs: 메인페이지에 구글ga 태그 추가

--- app/page.jsx
+++ app/page.jsx
@@ -8,9 +8,21 @@ import { visibility } from "../context/context";
 import Contact from "../components/Contact";
 import Snowfall from "react-snowfall";
 React.useLayoutEffect = React.useEffect;
+import Script from "next/script";
 function Page() {
   return (
     <div>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-36GQRC58RX"/>
+      <Script strategy="afterInteractive">  
+      {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-36GQRC58RX');
+      `
+      }
+      </Script>
       <ThemeProvider attribute="class">
         <Navbar visibility={visibility} />
         <Snowfall />
