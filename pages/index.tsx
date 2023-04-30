2916b4bb5d3fdc29a20b1070c22acff2bd3f9f08
parkgain <pgi082247@gmail.com>
2023-04-30 17:10:15 +0900

    feat: NextSEO 적용해서 SEO 점수를 100점으로 만듦

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,34 @@ import Header from "components/Header";
 import "../app/globals.css";
 import { Button, Space } from "antd";
 import About from "components/About/About";
+import { NextSeo } from "next-seo";
 
 const Page = () => {
   return (
-    <div className="background">
-      <div className="page_container flex-container">
-        <Header />
-        <Space wrap>
-          <Button type="primary" href="/projects">
-            포트폴리오 보러가기
-          </Button>
-        </Space>
-        <About />
-        <Contact />
+    <>
+      <NextSeo
+        title="박가인 포트폴리오"
+        description="개발자 포트폴리오 "
+        openGraph={{
+          type: "website",
+          url: "parkgaini.com",
+          title: "박가인 포트폴리오",
+          description: "개발자 박가인 포트폴리오",
+        }}
+      />
+      <div className="background">
+        <div className="page_container flex-container">
+          <Header />
+          <Space wrap>
+            <Button type="primary" href="/projects">
+              포트폴리오 보러가기
+            </Button>
+          </Space>
+          <About />
+          <Contact />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
