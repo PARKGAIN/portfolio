ebb3d362bc094bae1216f741df860a33a771255f
parkgain <pgi082247@gmail.com>
2023-07-12 23:56:47 +0900

    코드 정렬 위하여 prettier 설치함

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,9 +3,7 @@ import "../Header/index.css";
 
 const Header = () => {
   return (
-    <div
-      className={`margin-top-25 margin-bottom-10 font-size font-color ${karla.className}`}
-    >
+    <div className={`margin-top-25 margin-bottom-10 font-size font-color ${karla.className}`}>
       Hello,
       <br />I am parkgain~
     </div>
