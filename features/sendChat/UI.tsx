e5ead50e95614793b9e796027cd6e68d182d698c
parkgain <pgi082247@gmail.com>
2023-09-11 23:41:33 +0900

    채팅UI수정중

--- features/sendChat/UI.tsx
+++ features/sendChat/UI.tsx
@@ -1,5 +1,41 @@
-const SendChatUI = () => {
-  return <div></div>;
-};
+import { useState } from "react";
+import "./index.css";
 
+const SendChatUI = () => {
+  const [messages, setMessages] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (inputValue.trim() !== "") {
+      setMessages(inputValue);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        {messages.map((message, index) => (
+          <div key={index} className="message">
+            {message}
+          </div>
+        ))}
+      </div>
+      <div className="chat-input">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Type your message..."
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    </div>
+  );
+};
+// ffc1cc
 export default SendChatUI;
